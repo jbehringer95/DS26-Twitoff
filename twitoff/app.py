aa29323f2038d978ad590a04a08f84ff31330109
Justin C Behringer <63168114+jbehringer95@users.noreply.github.com>
2021-05-17 17:07:43 -0500

    Initial commit

---
+++ twitoff/app.py
@@ -0,0 +1,40 @@
+"""Main app/routing file for Twitoff"""
+
+from flask import Flask, render_template
+from .models import DB, User
+
+
+def create_app():
+    """Creates and configures an instance of the flask application"""
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    DB.init_app(app)
+
+    @app.route('/')
+    def root():
+        users = User.query.all()
+        return render_template("base.html", title="Home", users=users)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        users = User.query.all()
+        return render_template("base.html", title="Home", users=users)
+
+    @app.route('/populate')
+    def populate():
+        insert_users(["elonmusk", "jackblack"])
+        users = User.query.all()
+        return render_template("base.html", title="Home", users=users)
+
+    return app
+
+
+def insert_users(usernames):
+    for id_index, username in enumerate(usernames):
+        user = User(id=id_index, username=username)
+        DB.session.add(user)
+        DB.session.commit()
---
+++ twitoff/models.py
@@ -0,0 +1,33 @@
+"""SQLAlchemy models and database architecture"""
+
+from flask_sqlalchemy import SQLAlchemy
+
+DB = SQLAlchemy()
+
+
+# Creates a 'user' Table
+class User(DB.Model):
+    # id primary key column for 'user'
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    # username column for 'user'
+    username = DB.Column(DB.String, nullable=False)
+
+    def __repr__(self):
+        return f"<User: {self.username}>"
+
+
+# Creates a 'tweet' Table
+class Tweet(DB.Model):
+    # id primary key column for 'tweet'
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    # text column for 'tweet'
+    text = DB.Column(DB.Unicode(300))
+    # user_id foreign key column for 'tweet'
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey(
+        'user.id'), nullable=False)
+
+    ### TODO: STRETCH GOAL ###
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
+    def __repr__(self):
+        return f"<Tweet: {self.text}>"
