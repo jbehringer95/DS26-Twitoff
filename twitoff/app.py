69bd35225b3e2125c50f566d4b01f7801e9c7bff
Justin C Behringer <63168114+jbehringer95@users.noreply.github.com>
2021-05-20 12:02:15 -0500

    preparing to deploy application

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,7 +1,10 @@
 """Main app/routing file for Twitoff"""
 
-from flask import Flask, render_template
+from os import getenv
+from twitoff.predict import predict_user
+from flask import Flask, render_template, request
 from .models import DB, User
+from .twitter import add_or_update_user
 
 
 def create_app():
@@ -14,27 +17,75 @@ def create_app():
 
     @app.route('/')
     def root():
-        users = User.query.all()
-        return render_template("base.html", title="Home", users=users)
+        return render_template("base.html", title="Home", users=User.query.all())
+
+    @app.route('/compare', methods=["POST"])
+    def compare():
+        # getting users and hypothetical tweet from client
+        user0, user1 = sorted(
+            [request.values['user0'], request.values['user1']])
+        hypo_tweet_text = request.values["tweet_text"]
+
+        # stops clients from comparing same user
+        if user0 == user1:
+            message = "Cannot compare users to themselves!"
+
+        else:
+            prediction = predict_user(user0, user1, hypo_tweet_text)
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                hypo_tweet_text,
+                user1 if prediction else user0,
+                user0 if prediction else user1
+            )
+
+        return render_template('prediction.html', title='Prediction', message=message)
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values["user_name"]
+        try:
+            if request.method == "POST":
+                add_or_update_user(name)
+                message = f"User {name} successfully added!"
+
+            tweets = User.query.filter(User.username == name).one().tweets
+        except Exception as e:
+            message = f"Error adding {name}: {e}"
+            tweets = []
+
+        return render_template("user.html", title=name, tweets=tweets, message=message)
+
+    @app.route('/populate')
+    def populate():
+        add_or_update_user("garyvee")
+        add_or_update_user("mcuban")
+        return render_template("base.html", title="Home", users=User.query.all())
 
     @app.route('/reset')
     def reset():
         DB.drop_all()
         DB.create_all()
-        users = User.query.all()
-        return render_template("base.html", title="Home", users=users)
+        return render_template("base.html", title="Home", users=User.query.all())
 
-    @app.route('/populate')
-    def populate():
-        insert_users(["elonmusk", "jackblack"])
-        users = User.query.all()
-        return render_template("base.html", title="Home", users=users)
+    @app.route('/update')
+    def update():
+        for user in User.query.all():
+            add_or_update_user(user.username)
+
+        return render_template('base.html', title='All users tweets were updated!', users=User.query.all())
 
     return app
 
 
-def insert_users(usernames):
-    for id_index, username in enumerate(usernames):
-        user = User(id=id_index, username=username)
-        DB.session.add(user)
-        DB.session.commit()
+# def insert_users(usernames):
+#     for id_index, username in enumerate(usernames):
+#         user = User(id=id_index, username=username)
+#         DB.session.add(user)
+#         DB.session.commit()
+
+# def insert_tweet(tweets):
+#     for id_index, text in enumerate(tweets):
+#         tweet = Tweet(id=id_index, text=text, user_id=User.id)
+#         DB.session.add(tweet)
+#         DB.session.commit()
--- twitoff/models.py
+++ twitoff/models.py
@@ -1,33 +1,37 @@
-"""SQLAlchemy models and database architecture"""
+"""Structuring the DB and models"""
 
+from enum import unique
 from flask_sqlalchemy import SQLAlchemy
 
 DB = SQLAlchemy()
 
 
-# Creates a 'user' Table
+# Creates the User table in the DB
 class User(DB.Model):
-    # id primary key column for 'user'
+    # Setting up the id for the user
     id = DB.Column(DB.BigInteger, primary_key=True)
-    # username column for 'user'
-    username = DB.Column(DB.String, nullable=False)
+    # Setting up the username for the user
+    username = DB.Column(DB.String, nullable=False, unique=True)
+    # Stores most recent tweet_id
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
         return f"<User: {self.username}>"
 
 
-# Creates a 'tweet' Table
+# Creates the tweet table in the DB
 class Tweet(DB.Model):
-    # id primary key column for 'tweet'
+    # Setting up the tweet id for tweet
     id = DB.Column(DB.BigInteger, primary_key=True)
-    # text column for 'tweet'
+    # Text column for each tweet
     text = DB.Column(DB.Unicode(300))
-    # user_id foreign key column for 'tweet'
-    user_id = DB.Column(DB.BigInteger, DB.ForeignKey(
-        'user.id'), nullable=False)
-
-    ### TODO: STRETCH GOAL ###
+    # Stores numbers for user tweets
+    vect = DB.Column(DB.PickleType, nullable=False)
+    # user_id foreign key column for tweet
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'),
+                        nullable=False)
+    
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
-
+    
     def __repr__(self):
         return f"<Tweet: {self.text}>"
